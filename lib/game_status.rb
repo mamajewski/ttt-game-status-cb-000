9bc02eff2a2bc455c454b5bfc543ad31e3335d6f
Melissa Majewski <mreece85@gmail.com>
2017-11-19 22:26:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,43 @@ WIN_COMBINATIONS = [
   [1,4,7],
   [2,5,8]
 ]
+
+def full?(board)
+  return (position_taken?(board, 0) && position_taken?(board, 1) && position_taken?(board, 2) && position_taken?(board, 3) && position_taken?(board, 4) && position_taken?(board, 5) && position_taken?(board, 6) && position_taken?(board, 7) && position_taken?(board, 8))
+end
+
+def empty?(board)
+  return (!position_taken?(board, 0) && !position_taken?(board, 1) && !position_taken?(board, 2) && !position_taken?(board, 3) && !position_taken?(board, 4) && !position_taken?(board, 5) && !position_taken?(board, 6) && !position_taken?(board, 7) && !position_taken?(board, 8))
+end
+
+def won?(board)
+  if empty?(board)
+    return false
+  end
+
+  WIN_COMBINATIONS.each do |entry|
+    positions_taken = (position_taken?(board, entry[0]) && position_taken?(board, entry[1]))
+    positions_equal = ((board[entry[0]] == board[entry[1]]) && (board[entry[1]] == board[entry[2]]))
+    if (positions_taken && positions_equal)
+      return entry
+    end
+  end
+  return false
+end
+
+
+def draw?(board)
+  return (full?(board) == true && won?(board) == false)
+end
+
+def over?(board)
+  return (won?(board) || draw?(board) || full?(board))
+end
+
+def winner(board)
+  w = won?(board)
+  if w == false
+    return nil
+  end
+  return board[w[1]]
+end
